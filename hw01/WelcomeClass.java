d38ded52e2e1a33a2fc39988f67d7b602fd44685
ivanwzr919 <zhw218@lehigh.edu>
2014-09-10 15:36:13 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,16 @@
+public class Welcomeclass{
+
+  public static void main(String[] args){
+    
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-Z--H--W--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println(" This August my parents and I took a 10 days,6000 miles tour to Qinghai.");
+    System.out.println(" We spent a wonderful time in the highlands.");
+  }
+}
