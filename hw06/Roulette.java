745331ea395f0a9b9529c95f1297b0312646318f
ivanwzr919 <zhw218@lehigh.edu>
2014-10-13 01:07:24 +0000

    updated hw06

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -4,14 +4,14 @@
 //Professor Brian Chen 
 //Class CSE 002-112
 //CourseNumber Java Program
-//The program that has three boolean variables to each of which the value of true or false is randomly assigned. Then, at random, combine the three variables with random choices of && and || and stores the result.
+//The program is to analyze whether a roulette is easy to win or not
 import java.util.Scanner;
 public class Roulette{
      public static void main(String[] args){
      Scanner myScanner = new Scanner( System.in ); 
      System.out.print("Enter the times that you want to run the simulation- ");
          int Count=myScanner.nextInt();
-         int count=1,totalCount=1,bonus=0,win=0;
+         int count=1,totalCount=1,bonus=0,win=0,lose=0;
          while(totalCount<=Count){
          while(count<=100){
              int roulette=(int)(Math.random()*38)+1;
@@ -21,9 +21,11 @@ public class Roulette{
          }
          if(count>100){
             int totalBonus=bonus*36;
-            if(totalBonus>100){win=win+1;count=0;}
+            if(totalBonus>100){win=win+1;count=0;bonus=0;}
+            else{lose=lose+1;count=0;bonus=0;}
          }
          totalCount++;}
+         System.out.println("win= "+win+" ,lose= "+lose);
          if(win>=(Count/2)){System.out.println("You win.");}
          else{System.out.println("You lose.");}
      }
